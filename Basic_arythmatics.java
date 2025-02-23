347d599940dbcbdf567988504326133802c72cdf
yasath <wolfyasa22@gmail.com>
2025-02-23 09:33:05 +0530

    Add files via upload

---
+++ Basic_arythmatics.java
@@ -0,0 +1,15 @@
+public class Basic_arythmatics {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 12;
+        int sum = num1 - num2;
+        int addition = num1 + num2;
+        int subs = num1 / num2;
+        int multiply = num1 * num2;
+
+        System.out.println("Summation = " + sum);
+        System.out.println("Addition = " + addition);
+        System.out.println("Substraction = " + subs);
+        System.out.println("Multiplication = " + multiply);
+    }
+}
---
+++ New.java
@@ -0,0 +1,17 @@
+public class New {
+        public static void main(String[] args) {
+            int age = 22;
+            long population = 20000001;
+            float price = 20.50f;
+            double pi = 3.1456;
+            char grade = 'A';
+            boolean isJavaFun = true;
+
+            System.out.println("Integer (age): " + age);
+            System.out.println("Long (population): " + population);
+            System.out.println("Float (price): " + price);
+            System.out.println("Double (pi): " + pi);
+            System.out.println("Char (grade): " + grade);
+            System.out.println("Boolean (isJavaFun): " + isJavaFun);
+    }
+}
