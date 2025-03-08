887a05e600e8dc90b7e4b59a59446377a10d88c6
YasatH <wolfyasa22@gmail.com>
2025-03-08 18:55:57 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        int num=2;
+        if (num == 1){
+            System.out.println("one");
+        }else if (num ==2){
+            System.out.println("two");
+        }else {
+            System.out.println("other");
+        }
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,15 @@
+public class Test2 {
+    public static void main(String[] args) {
+        int num =5;
+        switch(num){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            default:
+                System.out.println("other");
+        }
+    }
+}
